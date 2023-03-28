49db9594583433b8ed885e3350f3edd4e36e9954
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-27 21:53:58 -0700

    new note button

--- scipts/script.js
+++ scipts/script.js
@@ -1,32 +1,49 @@
 // Dark Theme Button
-const darkbtn = document.querySelector(".button_darktheme")
-const aside = document.querySelector(".aside")
-const main = document.querySelector(".main")
-const placeholder = document.querySelector(".placeholder_textarea")
+const darkBtn = document.querySelector(".button_darktheme");
+const aside = document.querySelector(".aside");
+const main = document.querySelector(".main");
+const placeholder = document.querySelector(".placeholder_textarea");
 
 function darkTheme()
 {
     aside.classList.toggle("darktheme_lighter");
     main.classList.toggle("darktheme_deeper");
     placeholder.classList.toggle("darktheme_lighter");
-    if (darkbtn.textContent === "Dark Theme")
-        {darkbtn.textContent = "Light Theme"}
+    if (darkBtn.textContent === "Dark Theme")
+        {darkBtn.textContent = "Light Theme"}
     else
-        {darkbtn.textContent = "Dark Theme"}
+        {darkBtn.textContent = "Dark Theme"}
 }
 
-darkbtn.addEventListener("click", darkTheme)
+darkBtn.addEventListener("click", darkTheme);
 
 
 // Cancel Button
-const cancelbtn = document.querySelector(".button_cancel")
-const btnOption2 = document.querySelector(".button_option_2")
-const placeholderContainer = document.querySelector(".placeholder_div")
+const cancelBtn = document.querySelector(".button_cancel");
+const btnOption2 = document.querySelector(".button_option_2");
+const placeholderContainer = document.querySelector(".placeholder_div");
 
 function hideButton()
 {
-    btnOption2.classList.add("hide")
-    placeholderContainer.classList.add("hide")
+    btnOption2.classList.add("hide");
+    placeholderContainer.classList.add("hide");
 }
 
-cancelbtn.addEventListener("click", hideButton)
+cancelBtn.addEventListener("click", hideButton);
+
+
+// New Note button
+const newNoteBtn = document.querySelector(".button_newnote");
+const btnOption2Attribute = document.getElementsByClassName("button_option_2");
+
+function newNoteButton()
+{
+    if (btnOption2Attribute[0].className.includes("hide"))
+    {
+        btnOption2.classList.remove("hide");
+        placeholderContainer.classList.remove("hide");
+        placeholder.value = 'This is a placeholder';
+    }
+}
+
+newNoteBtn.addEventListener("click", newNoteButton);
